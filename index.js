d17b4c12356f25542d7d8f834a167b608a32441c
Nicola Martinez <nicoenrique_1994@hotmail.com>
2021-08-11 21:05:25 -0400

    Work done and first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let symbol = ["♦","♥","♠","♣"];
+let number = ["1","2","3","4","5","6","7","8","9","10","J","Q","K","A"];
+let randomsymbol = Math.floor(Math.random()* symbol.length);
+let randomnumber = Math.floor(Math.random()* number.length);
+
+document.querySelector(".top").innerHTML = symbol[randomsymbol];
+document.querySelector(".number").innerHTML = number[randomnumber];
+document.querySelector(".bottom").innerHTML = symbol[randomsymbol];
+
+let color = () => {
+    if(symbol[randomsymbol] == "♥" || symbol[randomsymbol] == "♦" ){
+        document.querySelector(".top").style.color = "red";
+        document.querySelector(".number").style.color = "red";
+        document.querySelector(".bottom").style.color = "red";
+    }
+}
+color();
